aa901d138dbae095fe0eb395c09e5edc48ee8630
Adrian Szymanowski <adriano100417@gmail.com>
2023-07-10 10:09:07 +0200

    Initial Commit

---
+++ FractalianGrapshNavigator/FractalianGrapshNavigatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FractalianGrapshNavigatorApp.swift
+//  FractalianGrapshNavigator
+//
+//  Created by Adrian Szymanowski on 10/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct FractalianGrapshNavigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
